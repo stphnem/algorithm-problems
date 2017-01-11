d5233d800d5cba8aa8766bced20725802b678282
Stephen Em <stphnem@gmail.com>
2017-01-10 18:21:21 -0800

    Finish InorderTraversal

---
+++ src/Trees/InorderTraversal.java
@@ -0,0 +1,34 @@
+package Trees;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class InorderTraversal {
+    public void inorderTraversal(TreeNode root) {
+        List<Integer> list = new ArrayList<>();
+        Stack<TreeNode> stack = new Stack<>();
+        TreeNode cur = root;
+        stack.push(cur);
+
+        while (!stack.isEmpty() || cur != null) {
+            while (cur != null) {
+                stack.push(cur);
+                cur = cur.left;
+            }
+
+            cur = stack.pop();
+            list.add(cur.val);
+            cur = cur.right;
+        }
+    }
+}
---
+++ src/Trees/ValidateBinarySearchTree.java
@@ -0,0 +1,16 @@
+package Trees;
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class ValidateBinarySearchTree {
+    public boolean isValidBST(TreeNode root) {
+        return false;
+    }
+}
