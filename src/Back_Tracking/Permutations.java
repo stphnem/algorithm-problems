00cd906161fe91801e38cbe0c59a708b2e136e22
Stephen Em <stphnem@gmail.com>
2017-03-01 11:42:01 -0800

    Created Permutations.java

---
+++ src/Back_Tracking/Permutations.java
@@ -0,0 +1,25 @@
+/**
+ * 46. Permutations
+ * https://leetcode.com/problems/permutations/?tab=Description
+ *
+ **/
+public class Solution {
+    public List<List<Integer>> permute(int[] nums) {
+        List<List<Integer>> ans = new ArrayList<>();
+        backtrack(ans, new ArrayList<>(), nums);
+        return ans;
+    }
+
+    public void backtrack(List<List<Integer>> ans, List<Integer> temp, int[] nums) {
+        if (temp.size() == nums.length) {
+            ans.add(new ArrayList<>(temp));
+        } else {
+            for (int i=0; i < nums.length; i++) {
+                if (temp.contains(nums[i])) continue;
+                temp.add(nums[i]);
+                backtrack(ans, temp, nums);
+                temp.remove(temp.size() - 1);
+            }
+        }
+    }
+}
